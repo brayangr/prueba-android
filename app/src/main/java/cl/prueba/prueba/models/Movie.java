f8984588f450402372609875cdc568697fae97fc
Brayan Gonzalez <bgonzalez@acid.cl>
2018-11-10 18:27:25 -0300

    feat(api-integration): Added model for movies and fixed typo in folder name

---
+++ app/src/main/java/cl/prueba/prueba/models/Movie.java
@@ -0,0 +1,150 @@
+package cl.prueba.prueba.models;
+
+import java.util.ArrayList;
+import java.util.Date;
+/**
+ * The Movie model implements the attributes that comes from themoviedb.org/3/movie/top_rated and
+ * themoviedb.org/3/movie/popular end points.
+ *
+ * @author  Brayan Gonzalez
+ * @version 1.0
+ * @since   2018-11-10
+ */
+public class Movie {
+    private int id;
+    private int voteCount;
+    private boolean video;
+    private float voteAverage;
+    private String title;
+    private String popularity;
+    private String originalLanguage;
+    private String originalTitle;
+    private String backdropPath;
+    private boolean adult;
+    private String overview;
+    private Date releaseDate;
+    private ArrayList<Genre> genres;
+
+    public Movie(int id, int voteCount, boolean video, float voteAverage, String title,
+                 String popularity, String originalLanguage, String originalTitle,
+                 String backdropPath, boolean adult, String overview, Date releaseDate,
+                 ArrayList<Genre> genres) {
+        this.id = id;
+        this.voteCount = voteCount;
+        this.video = video;
+        this.voteAverage = voteAverage;
+        this.title = title;
+        this.popularity = popularity;
+        this.originalLanguage = originalLanguage;
+        this.originalTitle = originalTitle;
+        this.backdropPath = backdropPath;
+        this.adult = adult;
+        this.overview = overview;
+        this.releaseDate = releaseDate;
+        this.genres = genres;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(int voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    public boolean isVideo() {
+        return video;
+    }
+
+    public void setVideo(boolean video) {
+        this.video = video;
+    }
+
+    public float getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(float voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(String popularity) {
+        this.popularity = popularity;
+    }
+
+    public String getOriginalLanguage() {
+        return originalLanguage;
+    }
+
+    public void setOriginalLanguage(String originalLanguage) {
+        this.originalLanguage = originalLanguage;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+    public boolean isAdult() {
+        return adult;
+    }
+
+    public void setAdult(boolean adult) {
+        this.adult = adult;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public ArrayList<Genre> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(ArrayList<Genre> genres) {
+        this.genres = genres;
+    }
+}
